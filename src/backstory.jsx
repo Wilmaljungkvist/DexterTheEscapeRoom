23d2504516cb234e6881a138a6845cf6f4263dc6
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-04-20 15:03:01 +0200

    add backstory

--- src/backstory.jsx
+++ src/backstory.jsx
@@ -7,12 +7,32 @@ const Backstory = () => {
 
   const story = [
     {
-      image: './img/kitchen.png',
-      text: 'I woke up early and went to the kitchen to make some coffee.'
+      image: './img/forrest.png',
+      text: 'Dexter är din katt som är en svart Cornish Rex. Han är din absoluta bästa vän, och det är lite sorgligt, men sant, eftersom du precis har flyttat till en ny stad och ännu inte hunnit skaffa många vänner än. Det var en vanlig dag, och du hade precis kommit hem mycket trött efter ett 8-timmars pass på ditt lagerjobb.'
+    },
+    {
+      image: './img/home.png',
+      text: 'Du möttes av Dexter vid dörren, som hälsade dig välkommen hem med ett glatt "meow". Du kände dig genast lite lugnare och gladare när du såg hans mjuka päls och hörde hans välkomnande ljud.'
+    },
+    {
+      image: './img/light.png',
+      text: 'Du satte dig ner på soffan och slappnade av en stund med Dexter bredvid dig.'
     },
     {
       image: './img/dark.png',
-      text: 'Suddenly, the power went out and the room was plunged into darkness.'
+      text: 'Du fastnade framför TV:n och insåg att tiden gick lite snabbare än du hade velat.'
+    },
+    {
+      image: './img/bedroom.png',
+      text: 'Snabbt förberedde du dig för sängen så att det inte skulle bli alltför sent, men du hann inte sova länge innan du hörde en duns. Du blev rädd eftersom du inte kunde hitta Dexter, som annars alltid låg vid dina fötter. Du reste dig upp och gick mot köket, där ljudet kom ifrån.'
+    },
+    {
+      image: './img/kitchen.png',
+      text: 'När du kom till köket såg nu något underligt, en del av tapeten hade rivits av och det visades nu en del av en dörr. Du bestämmer dig för att öppna dörren för att se om de är hit Dexter farit'
+    },
+    {
+      image: './img/stairs.png',
+      text: 'Till din fasa va de en trappa ner till en källare, mörkt va det också, men Dexter måste hittas tänkte du. Du hämtade en ficklampa, tog några djupa andetag och gick ner för den mörka trappan.'
     }
   ]
 
@@ -51,7 +71,7 @@ const Backstory = () => {
       <div
         style={{
           position: 'absolute',
-          top: '50%',
+          top: '85%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
           backgroundColor: 'rgba(255, 255, 255, 0.8)',
