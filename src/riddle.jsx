785dc732e4874bb6908909fecf0a4f4de31196b2
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-05-18 11:03:13 +0200

    add a bas to riddle

--- src/riddle.jsx
+++ src/riddle.jsx
@@ -1,7 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 
-function RiddleGame () {
+function RiddleGame ({ riddleAnswer, whenSolved }) {
+const [answer, setAnswer] = useState('')
+
+function handleSubmit () {
+    alert('You submitted')
+}
+
+function handleInputChange () {
+    alert('You changed the answer')
+}
+return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='answerInput'>Lös gåtan för att komma in i skåpet: </label>
+            <input type="text" id="answerInput" value={answer} onChange={handleInputChange} />
+            <button type="submit">Är det rätt svar?</button>
+        </form>
+    </div>
+)
 
 }
 
