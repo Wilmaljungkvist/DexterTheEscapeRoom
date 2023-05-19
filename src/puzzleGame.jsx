65cce271768961557f3c6cc606f95930374b608b
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-05-19 12:51:50 +0200

    JsDoc Riddle.jsx and PuzzleGame.jsx

--- src/puzzleGame.jsx
+++ src/puzzleGame.jsx
@@ -1,12 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react'
 
 /**
+ * PuzzleGame component represents a puzzle game.
  *
- * @param root0
- *
- * @param root0.onPuzzleSolved
- *
- * @returns {JSX.Element} - The JSX element representing the Backstory component.
+ * @param {object} props - The props object.
+ * @param {Function} props.onPuzzleSolved - Callback function called when the puzzle is solved.
+ * @returns {JSX.Element} - The JSX element representing the PuzzleGame component.
  */
 function PuzzleGame ({ onPuzzleSolved }) {
   const [puzzle, setPuzzle] = useState([])
@@ -24,7 +24,9 @@ function PuzzleGame ({ onPuzzleSolved }) {
   }, [puzzle, onPuzzleSolved])
 
   /**
+   * Creates a new puzzle.
    *
+   * @returns {number[][]} - The new puzzle.
    */
   function createPuzzle () {
     const numbers = [null, 1, 3, 4, 2, 5, 7, 8, 6]
@@ -39,7 +41,9 @@ function PuzzleGame ({ onPuzzleSolved }) {
   }
 
   /**
+   * Checks if the puzzle is solved.
    *
+   * @returns {boolean} - True if the puzzle is solved, false otherwise.
    */
   function checkSolved () {
     const flatPuzzle = puzzle.flat()
@@ -52,9 +56,10 @@ function PuzzleGame ({ onPuzzleSolved }) {
   }
 
   /**
+   * Handles the move of a puzzle piece.
    *
-   * @param row
-   * @param col
+   * @param {number} row - The row index of the clicked piece.
+   * @param {number} col - The column index of the clicked piece.
    */
   function handleMove (row, col) {
     if (solved) {
@@ -75,9 +80,10 @@ function PuzzleGame ({ onPuzzleSolved }) {
 
     if (
       (row === emptyRow && Math.abs(col - emptyCol) === 1) ||
-            (col === emptyCol && Math.abs(row - emptyRow) === 1)
+      (col === emptyCol && Math.abs(row - emptyRow) === 1)
     ) {
-      [newPuzzle[row][col], newPuzzle[emptyRow][emptyCol]] = [newPuzzle[emptyRow][emptyCol],
+      [newPuzzle[row][col], newPuzzle[emptyRow][emptyCol]] = [
+        newPuzzle[emptyRow][emptyCol],
         newPuzzle[row][col]
       ]
       setPuzzle(newPuzzle)
@@ -89,40 +95,42 @@ function PuzzleGame ({ onPuzzleSolved }) {
   }
 
   /**
+   * Renders a puzzle cell.
    *
-   * @param row
-   * @param col
-   * @param value
+   * @param {number} row - The row index of the cell.
+   * @param {number} col - The column index of the cell.
+   * @param {number|null} value - The value of the cell.
+   * @returns {JSX.Element} - The JSX element representing the puzzle cell.
    */
   function renderCell (row, col, value) {
     return (
-            <div
-                className="cell"
-                key={`${row}-${col}`}
-                onClick={() => handleMove(row, col)}
-                style={{
-                  opacity: value === null ? 0 : 1,
-                  cursor: solved ? 'default' : 'pointer'
-                }}
-            >
-                {value}
-            </div>
+      <div
+        className="cell"
+        key={`${row}-${col}`}
+        onClick={() => handleMove(row, col)}
+        style={{
+          opacity: value === null ? 0 : 1,
+          cursor: solved ? 'default' : 'pointer'
+        }}
+      >
+        {value}
+      </div>
     )
   }
 
   return (
-        <div className="puzzle">
-            <p>Solve the puzzle!</p>
-            <div className="board">
-                {puzzle.map((row, rowIndex) =>
-                    <div className="row" key={rowIndex}>
-                        {row.map((value, colIndex) =>
-                          renderCell(rowIndex, colIndex, value)
-                        )}
-                    </div>
-                )}
-            </div>
-        </div>
+    <div className="puzzle">
+      <p>Solve the puzzle!</p>
+      <div className="board">
+        {puzzle.map((row, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {row.map((value, colIndex) =>
+              renderCell(rowIndex, colIndex, value)
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/riddle.jsx
+++ src/riddle.jsx
@@ -2,18 +2,21 @@
 import React, { useState } from 'react'
 
 /**
+ * RiddleGame component represents a riddle game.
  *
- * @param root0
- * @param root0.riddleAnswer
- * @param root0.whenSolved
- * @param root0.question
+ * @param {object} props - The props object.
+ * @param {string} props.riddleAnswer - The correct answer to the riddle.
+ * @param {Function} props.whenSolved - Callback function called when the riddle is solved.
+ * @param {string} props.question - The riddle question.
+ * @returns {JSX.Element} - The JSX element representing the About component.
  */
 function RiddleGame ({ riddleAnswer, whenSolved, question }) {
   const [answer, setAnswer] = useState('')
 
   /**
+   * Handles the form submission.
    *
-   * @param event
+   * @param {Event} event - The form submission event.
    */
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -25,21 +28,33 @@ function RiddleGame ({ riddleAnswer, whenSolved, question }) {
   }
 
   /**
+   * Handles the input change.
    *
-   * @param event
+   * @param {Event} event - The input change event.
    */
   const handleInputChange = (event) => {
     setAnswer(event.target.value)
   }
+
   return (
-        <div className="riddle">
-            <p className="riddlequestion">{question}</p>
-            <form className="riddlesubmit" onSubmit={handleSubmit}>
-                <label className="riddlelabel" htmlFor='answerInput'>Lös gåtan för att komma in i skåpet: </label>
-                <input className="riddleinput" type="text" id="answerInput" value={answer} onChange={handleInputChange} />
-                <button className="riddlebutton" type="submit">Är det rätt svar?</button>
-            </form>
-        </div>
+    <div className="riddle">
+      <p className="riddlequestion">{question}</p>
+      <form className="riddlesubmit" onSubmit={handleSubmit}>
+        <label className="riddlelabel" htmlFor="answerInput">
+          Lös gåtan för att komma in i skåpet:
+        </label>
+        <input
+          className="riddleinput"
+          type="text"
+          id="answerInput"
+          value={answer}
+          onChange={handleInputChange}
+        />
+        <button className="riddlebutton" type="submit">
+          Är det rätt svar?
+        </button>
+      </form>
+    </div>
   )
 }
 
