1fd24de72f09956e78e77ca056d047dd908ad310
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-05-19 18:19:01 +0200

    add automatic test

--- test/components.test.js
+++ test/components.test.js
@@ -1,17 +1,26 @@
-import React from 'react'
 import renderer from 'react-test-renderer'
+import { render, fireEvent } from '@testing-library/react'
 import LastRoom from '../src/components/LastRoom'
 
-test('password submission with correct password should open the safe', () => {
+test('renders LastRoom component correctly', () => {
   const component = renderer.create(<LastRoom />)
-  const instance = component.root
-  const event = { target: { value: '6743' }, preventDefault: jest.fn() }
-
-  const inputElement = instance.findByProps({ id: 'passwordInput' })
-  inputElement.props.onChange(event)
-
-  const buttonElement = instance.findByProps({ type: 'submit' })
-  buttonElement.props.onClick(event)
-
-  expect(instance.findByProps({ className: 'safe' })).toBeFalsy()
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
+
+test('entering incorrect password displays error message', () => {
+  const { getByAltText, getByLabelText, getByText } = render(<LastRoom />)
+  const safe = getByAltText('safe')
+
+  fireEvent.click(safe)
+
+  const passwordInput = getByLabelText('Enter password:')
+  const okButton = getByText('OK')
+
+  fireEvent.change(passwordInput, { target: { value: '1234' } })
+  fireEvent.click(okButton)
+
+  const errorMessage = getByText('Fel lösenord, försök igen.')
+  expect(errorMessage).toBeTruthy()
+})
+
