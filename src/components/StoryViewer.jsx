bcecac9a4ecc0f3b7b857e2ac3884ebe8fa24426
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-07-21 09:47:48 +0200

    Make component for viewing stories

--- src/components/StoryViewer.jsx
+++ src/components/StoryViewer.jsx
@@ -1,59 +1,22 @@
 /* eslint-disable jsdoc/no-undefined-types */
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react'
-// eslint-disable-next-line no-unused-vars
-import FirstRoom from './FirstRoom'
 
 /**
- * Component for showing the Backstory to the escape room.
+ * Component for viewing the stories in Backstory and Enstory.
  *
- * @returns {JSX.Element} - the JSX element representing the Backstory component.
+ * @param root0
+ * @param root0.image
+ * @param root0.text
+ * @returns {JSX.Element} - the JSX element representing the StoryViewer component.
  */
-const Backstory = () => {
-  const [currentStep, setCurrentStep] = useState(0)
+function StoryViewer ({ image, text }) {
   const [showText, setShowText] = useState(false)
   const [displayText, setDisplayText] = useState('')
-  const [showFirstRoom, setShowFirstRoom] = useState(false)
-
-  const story = [
-    {
-      image: './img/forrest.png',
-      text: 'Dexter är din katt som är en svart Cornish Rex. Han är din absoluta bästa vän, det är lite sorgligt men sant. Det är dock inte så konstigt eftersom du precis flyttat till en ny stad och ännu inte hunnit skaffa några vänner än. Det var en vanlig dag och du hade precis kommit hem mycket trött efter ett 8-timmars pass på ditt lagerjobb.'
-    },
-    {
-      image: './img/home.png',
-      text: 'Du möttes av Dexter vid dörren, som hälsade dig välkommen hem med ett glatt "meow". Du kände dig genast lite lugnare och gladare.'
-    },
-    {
-      image: './img/light.png',
-      text: 'Du bestämde dig för att se din favorit serie och satte dig ner på soffan.'
-    },
-    {
-      image: './img/dark.png',
-      text: '6 timmar senare hade du sett en hel säsong och insåg att tiden gick lite snabbare än du hade velat.'
-    },
-    {
-      image: './img/bedroom.png',
-      text: 'Snabbt förberedde du dig för sängen så att det inte skulle bli alltför sent, men du hann inte sova länge innan du hörde en duns. Du blev rädd eftersom du inte kunde hitta Dexter, som annars alltid låg vid dina fötter. Du reste dig upp och gick mot köket, där ljudet kom ifrån.'
-    },
-    {
-      image: './img/kitchen.png',
-      text: 'När du kom till köket såg nu något underligt, en del av tapeten hade rivits av och det visades nu en del av en dörr. Du bestämmer dig för att öppna dörren för att se om de är hit Dexter försvunnit.'
-    },
-    {
-      image: './img/stairs.png',
-      text: 'Till din fasa va de en trappa ner till en källare, mörkt va det också, men Dexter måste hittas tänkte du. Du hämtade en ficklampa, tog några djupa andetag och gick ner för den mörka trappan.'
-    },
-    {
-      image: './img/basement.png',
-      text: 'När du kom till botten av trappan befann du dig i en gammal källare. Det såg ut som om ingen hade varit där på flera år. Det va tomt, men något kändes fel, och du hade en känsla av att det fanns något mer som gömde sig där. Källardörren stängdes med en smäll, och du sprang snabbt upp för att försöka ta dig ut. Men du insåg snart att dörren var låst och att du behövde hitta en annan väg ut, men också hitta Dexter. Dina enda ledtrådar var ytterligare en låst dörr framför dig och en vält färgburk med tassavtryck på golvet som visade att Dexter hade gått genom den dörren. “Bara att börja leta efter en väg ut” tänkte du, ovetandes om det stora äventyret framför dig.'
-    }
-  ]
 
   useEffect(() => {
     setShowText(true)
     if (showText) {
-      const text = story[currentStep].text
       const timeoutId = setTimeout(() => {
         if (displayText.length < text.length) {
           setDisplayText(text.slice(0, displayText.length + 1))
@@ -63,61 +26,16 @@ const Backstory = () => {
       }, 20)
       return () => clearTimeout(timeoutId)
     }
-  }, [showText, currentStep, displayText, story])
-
-  /**
-   * Handles if next button is pressed.
-   */
-  const handleNext = () => {
-    setCurrentStep(0)
-    setCurrentStep(currentStep + 1)
-    setShowText(true)
-    setDisplayText('')
-  }
-
-  /**
-   * Handle if start button is pressed.
-   */
-  const handleStart = () => {
-    setShowFirstRoom(true)
-    setCurrentStep(7)
-  }
-
-  if (showFirstRoom) {
-    return <FirstRoom />
-  }
+  }, [showText, displayText, text])
 
   return (
     <div style={{ position: 'relative', height: '100vh' }}>
-      <img
-        className="story-image"
-        src={story[currentStep].image}
-        alt="Bilder för bakgrundshistorian"
-      />
-      <div
-        className="story-div"
-      >
+      <img className="story-image" src={image} alt="Story Image" />
+      <div className="story-div">
         <p className="backstory-text">{displayText}</p>
       </div>
-      {currentStep < story.length - 1
-        ? (
-          <button
-            className="start-next-btn"
-            onClick={handleNext}
-          >
-            Nästa
-          </button>
-          )
-        : (
-          <button
-            className="start-next-btn"
-            onClick={handleStart}
-          >
-            Starta
-          </button>
-          )}
     </div>
   )
 }
 
-export default Backstory
+export default StoryViewer
