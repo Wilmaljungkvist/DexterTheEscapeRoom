1aeb6bbae5b1f8dbc6f3e5bf15648f6b1077ee76
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-07-21 11:03:15 +0200

    Update docs in StoryButton

--- src/components/StoryButton.jsx
+++ src/components/StoryButton.jsx
@@ -5,9 +5,10 @@ import React from 'react'
 /**
  * Component for the buttons in the Endstory and Backstory.
  *
- * @param root0
- * @param root0.onClick
- * @param root0.label
+ * @param {object} props - The component props.
+ * @param {Function} props.onClick - The click event handler for the button.
+ * @param {string} props.label - The label/text to be displayed on the button.
+ * @returns {JSX.Element} - The JSX element representing the ButtonComponent.
  */
 function ButtonComponent ({ onClick, label }) {
   return (
