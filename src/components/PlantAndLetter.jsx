89809cf2cfb2790b9af776466df7043d06c47679
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-07-25 19:26:30 +0200

    JsDoc PlantAndLetter

--- src/components/PlantAndLetter.jsx
+++ src/components/PlantAndLetter.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable jsdoc/no-undefined-types */
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 
 /**
+ * A React component that represents a plant and a letter.
  *
- * @param root0
- * @param root0.plantPressed
- * @param root0.plantClicked
+ * @param {object} props - The props object.
+ * @param {Function} props.plantPressed - Callback function called when the plant is pressed.
+ * @param {Function} props.plantClicked - Callback function called when the plant is clicked.
+ * @returns {JSX.Element} - The JSX element representing the PlantAndLetter component.
  */
 function PlantAndLetter ({ plantPressed, plantClicked }) {
   const [showLetter, setShowLetter] = useState(false)
